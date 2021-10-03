530ba537ac4b3aa0e69106164dca37755aed1813
Kiki Pratiwi <itsme.kikipratiwi@gmail.com>
2021-10-03 13:03:47 +0700

    feat(invitation): change invitation time

--- components/organisms/invitation.js
+++ components/organisms/invitation.js
@@ -79,13 +79,13 @@ const OrganismInvitation = () => {
 									text="Sesi 1: Kolega dari Orang Tua"
 									additionalSize={-1}
 								/>
-								<AtomText text="10.30 - 11.30 WIB" additionalSize={-1} />
+								<AtomText text="10.30 - 12.00 WIB" additionalSize={-1} />
 								<AtomText
 									text="Sesi 2: Rekan dari Kedua Mempelai"
 									additionalSize={-1}
 									style={{ marginTop: '.5rem' }}
 								/>
-								<AtomText text="11.30 - 13.45 WIB" additionalSize={-1} />
+								<AtomText text="12.00 - 13.45 WIB" additionalSize={-1} />
 							</Col>
 						</Row>
 
